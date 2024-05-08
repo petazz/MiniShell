d23c5e6fd9d50358ae8e85002f5e37bd3f736f2e
Pedro Gonzalez Cepeda <pgonzal2@c1r4s5.42malaga.com>
2024-05-08 21:34:19 +0200

    estructura inicializada

--- src/main.c
+++ src/main.c
@@ -1,4 +1,31 @@
-int main(void)
+int main(int argc, char **argv, char **envp)
 {
-	return 0;
+    (void)argc;
+    (void)argv;
+    char **mock_args = malloc(sizeof(char*) * 10);
+    mock_args[0] = "ls";
+    mock_args[1] = "-l";
+    mock_args[2] = 0;
+    t_cmd mock_ls = {
+        .path = "/bin/ls",
+        .arg = mock_args,
+        .len_arg = 2
+    };
+    t_msh mock = {
+        .cmd = &mock_ls,
+        .len_cmds = 1,
+        .envp = envp,
+        .fdinfile = -1,
+        .fdoutfile = -1,
+    };
+    //(void)mock;
+    ft_echo(mock.cmd->arg);
+    // char *line = readline("minishell$ ");
+    // while (line)
+    // {
+    //  add_history(line);
+    //  free(line);
+    //  line = readline("minishell$ ");
+    // }
+    return (0);
 }
